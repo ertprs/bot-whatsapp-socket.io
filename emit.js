2a1f39f7c35d538d1f3f5136836d1b3da52087fe
Marcony Caldeira <marconycaldeira@gmail.com>
2021-02-26 17:22:01 -0300

    Update emit.js

--- emit.js
+++ emit.js
@@ -2,6 +2,6 @@ const io = require("socket.io-client");
 var socket = io.connect("http://localhost:3000");
 
 socket.emit("received", {
-    number: '553888144175@c.us',
+    number: '555555555555@c.us',
     content: 'Esta é uma mensagem enviada através de socker.io'
 });
